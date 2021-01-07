26f2fb3b72961f17af7c5e6e9962f9448f33b8b4
Jose Pablo Wenzel <devwenzel@gmail.com>
2021-01-07 09:46:07 -0300

    Add simple jump only when grounded

--- Assets/Scripts/Main Character/PlayerController.cs
+++ Assets/Scripts/Main Character/PlayerController.cs
@@ -1,23 +1,51 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] bool isControllable = true;
+
     [SerializeField] float walkingSpeed = 5f;
+    [SerializeField] float jumpForce = 8f;
 
     private Rigidbody2D myRigidbody2D;
+    private CapsuleCollider2D feetCollider;
+
+    private bool isGrounded;
 
     private void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        feetCollider = GetComponentInChildren<CapsuleCollider2D>();
     }
 
     private void Update()
     {
+        CheckGrounded();
+
         if (isControllable)
         {
             MoveSideways(walkingSpeed);
+            Jump();
         }
+    }
+
+    private void CheckGrounded()
+    {
+        isGrounded = feetCollider.IsTouchingLayers(LayerMask.GetMask("Stepable"));
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            ThrustUpwards();
+        }
+    }
+
+    private void ThrustUpwards()
+    {
+        myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, jumpForce);
     }
 
     private void MoveSideways(float movingSpeed)
