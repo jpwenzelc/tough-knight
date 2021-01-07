825bd98a8cb2546e5556a26ece45b2a1f649d696
Jose Pablo Wenzel <devwenzel@gmail.com>
2021-01-07 09:05:25 -0300

    Add sideways movement with tuneable speed

---
+++ Assets/Scripts/Main Character/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] bool isControllable = true;
+    [SerializeField] float walkingSpeed = 5f;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (isControllable)
+        {
+            MoveSideways(walkingSpeed);
+        }
+    }
+
+    private void MoveSideways(float movingSpeed)
+    {
+        float horizontalThrust = Input.GetAxisRaw("Horizontal");
+
+        myRigidbody2D.velocity = new Vector2(horizontalThrust * movingSpeed
+                                             ,myRigidbody2D.velocity.y);
+    }
+}
